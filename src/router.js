b159270492799b4c30f73fecd3e50fb279ab36e5
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-08-08 23:34:16 -0300

    init project

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HomeRoute',
+      component: Home
+    },
+    {
+      path: '/quiz',
+      name: 'PlanetsQuizRoute',
+      // route level code-splitting
+      // this generates a separate chunk (planetsQuiz.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "planetsQuiz" */ './views/PlanetsQuiz.vue')
+    }
+  ]
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1> Star Wars Quiz! </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  components: {}
+}
+</script>
