5cd27548c8ae073cf1f42c3e5ef55ffdc4df28fd
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-08-28 08:41:34 -0300

    add QuestionText component and its test

---
+++ src/components/QuestionText.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="text-center">What planet was {{ person }} born on?</p>
+</template>
+
+<script>
+export default {
+  name: 'QuestionText',
+  props: {
+    person: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ tests/components/QuestionText.spec.js
@@ -0,0 +1,41 @@
+import { mount } from '@vue/test-utils'
+import QuestionText from '../../src/components/QuestionText'
+
+describe('QuestionText component', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(QuestionText)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
+
+describe('Test QuestionText component with nothing argument', () => {
+  const wrapper = mount(QuestionText)
+
+  test('should be the person prop with a empty string', () => {
+    expect(wrapper.props('person')).toBe('')
+  })
+
+  test('should be "What planet was  born on?"', () => {
+    expect(wrapper.text()).toBe('What planet was  born on?')
+  })
+})
+
+describe('Test QuestionText component with Luke Skywalker person argument', () => {
+  test('the person prop should be Luke Skywalker', () => {
+    const wrapper = mount(QuestionText, {
+      propsData: {
+        person: 'Luke Skywalker'
+      }
+    })
+    expect(wrapper.props('person')).toBe('Luke Skywalker')
+  })
+
+  test('should be "What planet was Luke Skywalker born on?"', () => {
+    const wrapper = mount(QuestionText, {
+      propsData: {
+        person: 'Luke Skywalker'
+      }
+    })
+    expect(wrapper.text()).toBe('What planet was Luke Skywalker born on?')
+  })
+})
