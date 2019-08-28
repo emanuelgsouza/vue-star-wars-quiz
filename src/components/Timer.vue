6f8d5ce7ff2fe80aaa30607b9527ed4dbf784051
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-08-28 09:02:03 -0300

    add timer component

---
+++ src/components/Timer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="timer">
+    <span class="icon">
+      <i class="far fa-clock"></i>
+    </span>
+    <span class="text">
+      00:00
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timer'
+}
+</script>
