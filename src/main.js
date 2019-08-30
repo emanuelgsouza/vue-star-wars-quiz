b30641b0170582eababadeb843d38f9e8a7c266d
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-08-30 18:54:24 -0300

    remove use of graphql

--- src/main.js
+++ src/main.js
@@ -2,18 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { install as installApollo, apolloProvider } from './support/apollo'
 
 import './assets/css/main.css'
 import './registerServiceWorker'
-
-installApollo(Vue)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-  apolloProvider
+  render: h => h(App)
 }).$mount('#app')
