a85ef0476830daa9f9e1298c3e7642340e4ea82d
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-08-30 16:55:30 -0300

    add open Graphql server to Star Wars API

--- src/support/apollo.js
+++ src/support/apollo.js
@@ -1,12 +1,11 @@
 import VueApollo from 'vue-apollo'
 import ApolloClient from 'apollo-boost'
-// import InMemoryCache from 'apollo-cache-memory'
 
-// const cache = new InMemoryCache()
+const isDev = process.env.NODE_ENV === 'development'
 
 export const graphqlClient = new ApolloClient({
-  // cache,
-  uri: 'http://localhost:9999'
+  // https://github.com/graphql/swapi-graphql/issues/83
+  uri: isDev ? 'http://localhost:9999' : 'https://swapi.apis.guru/'
 })
 
 export const apolloProvider = new VueApollo({
