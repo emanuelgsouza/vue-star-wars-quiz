b466ba4ddb5dbd6f22742f1b25a146e046221da3
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-02-12 23:00:36 -0300

    test(Steps): refactor tests

--- tests/components/Steps.spec.js
+++ tests/components/Steps.spec.js
@@ -1,33 +1,44 @@
 import { mount } from '@vue/test-utils'
 import Steps from '../../src/components/Steps'
 
-describe('Test Steps component with nothing argument', () => {
-  test('should be 0/0 string', () => {
-    const wrapper = mount(Steps)
-    expect(wrapper.text()).toMatch('0/0')
+describe('Steps component', () => {
+  describe('without any argument', () => {
+    it('should be 0/0 string', () => {
+      const wrapper = mount(Steps)
+
+      expect(wrapper.text()).toMatch('0/0')
+    })
+
+    it('should be the steps and actualStep props are empty (0)', () => {
+      const wrapper = mount(Steps)
+
+      expect(wrapper.props('steps')).toBe(0)
+      expect(wrapper.props('actualStep')).toBe(0)
+    })
   })
 
-  test('should be the steps and actualStep props are empty (0)', () => {
-    const wrapper = mount(Steps)
-    expect(wrapper.props('steps')).toBe(0)
-    expect(wrapper.props('actualStep')).toBe(0)
-  })
-})
+  describe('with arguments', () => {
+    it('should be 1/10 string when passing the 1 as step and 10 as actualStep', () => {
+      const wrapper = mount(Steps, {
+        propsData: {
+          steps: 10,
+          actualStep: 1
+        }
+      })
 
-const propsData = {
-  steps: 10,
-  actualStep: 1
-}
+      expect(wrapper.text()).toMatch('1/10')
+    })
 
-describe('Test Steps component with arguments 1 and 10', () => {
-  const wrapper = mount(Steps, { propsData })
+    it('should be populate the step and actual step properties', () => {
+      const wrapper = mount(Steps, {
+        propsData: {
+          steps: 8,
+          actualStep: 4
+        }
+      })
 
-  test('should be 1/10 string', () => {
-    expect(wrapper.text()).toMatch('1/10')
-  })
-
-  test('should be the steps 1 and actualStep 10', () => {
-    expect(wrapper.props('actualStep')).toBe(1)
-    expect(wrapper.props('steps')).toBe(10)
+      expect(wrapper.props('actualStep')).toBe(4)
+      expect(wrapper.props('steps')).toBe(8)
+    })
   })
 })
