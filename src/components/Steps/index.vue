5ace69a2ba10f10d205c2c875e4a9d2cc8e519df
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-02-15 22:41:17 -0300

    refactor(step): turn Step component as functional

--- src/components/Steps/index.vue
+++ src/components/Steps/index.vue
@@ -1,7 +1,7 @@
-<template>
+<template functional>
   <div class="steps">
     <p class="text-center">
-      <span class="left">{{ actualStep }}</span>/<span class="right">{{ steps }}</span>
+      <span class="left">{{ props.actualStep }}</span>/<span class="right">{{ props.steps }}</span>
     </p>
   </div>
 </template>
@@ -9,6 +9,7 @@
 <script>
 export default {
   name: 'Steps',
+  functional: true,
   props: {
     steps: {
       type: Number,
