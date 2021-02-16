bd6cd349d36a1383357fbfc8b3fbe24eb9fb6a5d
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-02-15 22:40:12 -0300

    test(step): refactor tests to test only the output

--- src/components/Steps/__tests__/Steps.test.js
+++ src/components/Steps/__tests__/Steps.test.js
@@ -2,43 +2,20 @@ import { mount } from '@vue/test-utils'
 import Steps from '../index.vue'
 
 describe('Steps component', () => {
-  describe('without any argument', () => {
-    it('should be 0/0 string', () => {
-      const wrapper = mount(Steps)
+  it('should render the 0/0 text when not passing any argument', () => {
+    const wrapper = mount(Steps)
 
-      expect(wrapper.text()).toMatch('0/0')
-    })
-
-    it('should be the steps and actualStep props are empty (0)', () => {
-      const wrapper = mount(Steps)
-
-      expect(wrapper.props('steps')).toBe(0)
-      expect(wrapper.props('actualStep')).toBe(0)
-    })
+    expect(wrapper.text()).toMatch('0/0')
   })
 
-  describe('with arguments', () => {
-    it('should be 1/10 string when passing the 1 as step and 10 as actualStep', () => {
-      const wrapper = mount(Steps, {
-        propsData: {
-          steps: 10,
-          actualStep: 1
-        }
-      })
-
-      expect(wrapper.text()).toMatch('1/10')
+  it('should render the corresponding text when passing the 1 as step and 10 as actualStep', () => {
+    const wrapper = mount(Steps, {
+      propsData: {
+        steps: 10,
+        actualStep: 1
+      }
     })
 
-    it('should be populate the step and actual step properties', () => {
-      const wrapper = mount(Steps, {
-        propsData: {
-          steps: 8,
-          actualStep: 4
-        }
-      })
-
-      expect(wrapper.props('actualStep')).toBe(4)
-      expect(wrapper.props('steps')).toBe(8)
-    })
+    expect(wrapper.text()).toMatch('1/10')
   })
 })
