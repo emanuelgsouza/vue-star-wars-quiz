a28a0881c6b977b5fcce5d3666dff63cf81cf8cc
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-02-15 22:59:55 -0300

    test(question-text): add tests to emit functionality and no argument

--- src/components/QuestionOptions/__tests__/Item.test.js
+++ src/components/QuestionOptions/__tests__/Item.test.js
@@ -1,13 +1,13 @@
 import { mount } from '@vue/test-utils'
 import QuestionItem from '../item.vue'
 
-describe('Test QuestionItem with specific option', () => {
+describe('QuestionItem component', () => {
   const option = {
     label: 'Tatooine',
     value: 'Tatooine'
   }
 
-  test('should be Tatooine as label and value and selected', () => {
+  it('should be Tatooine as label and value and selected', () => {
     const wrapper = mount(QuestionItem, {
       propsData: {
         option,
@@ -21,7 +21,7 @@ describe('Test QuestionItem with specific option', () => {
     expect(wrapper.vm.isSelected).toBe(true)
   })
 
-  test('should be Tatooine as label and value but not selected', () => {
+  it('should be Tatooine as label and value but not selected', () => {
     const wrapper = mount(QuestionItem, {
       propsData: {
         option,
@@ -33,5 +33,48 @@ describe('Test QuestionItem with specific option', () => {
     expect(wrapper.vm.value).toBe('Tatooine')
     expect(wrapper.vm.label).toBe('Tatooine')
     expect(wrapper.vm.isSelected).toBe(false)
+  })
+
+  it('should emit the input event when click on the label', async () => {
+    const wrapper = mount(QuestionItem, {
+      propsData: {
+        option,
+        selected: 'Rio de Janeiro'
+      }
+    })
+
+    await wrapper.trigger('click')
+
+    expect(wrapper.emitted('selected')).not.toBeUndefined()
+    expect(wrapper.emitted('selected')[0]).toEqual([option])
+  })
+
+  it('should render the correct label as text', async () => {
+    const wrapper = mount(QuestionItem, {
+      propsData: {
+        option,
+        selected: 'Rio de Janeiro'
+      }
+    })
+
+    await wrapper.trigger('click')
+
+    expect(wrapper.text()).toBe(option.label)
+  })
+
+  describe('render without arguments', () => {
+    it('should render an empty text', () => {
+      const wrapper = mount(QuestionItem)
+
+      expect(wrapper.text()).toBe('')
+    })
+
+    it('should not emit the selected event', async () => {
+      const wrapper = mount(QuestionItem)
+
+      await wrapper.trigger('click')
+
+      expect(wrapper.emitted('selected')).toBeUndefined()
+    })
   })
 })
