8ef29d9db301426e3fa6bcac8a9518a95e542fc6
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-02-15 22:45:10 -0300

    test(question-text): refactor QuestionText tests to only tests the output

--- src/components/QuestionText/__tests__/Question.test.js
+++ src/components/QuestionText/__tests__/Question.test.js
@@ -1,29 +1,14 @@
 import { mount } from '@vue/test-utils'
 import QuestionText from '../index.vue'
 
-describe('Test QuestionText component with nothing argument', () => {
-  const wrapper = mount(QuestionText)
+describe('QuestionText component', () => {
+  it('should render "What planet was  born on?" when no pass any argument', () => {
+    const wrapper = mount(QuestionText)
 
-  test('should be the person prop with a empty string', () => {
-    expect(wrapper.props('person')).toBe('')
-  })
-
-  test('should be "What planet was  born on?"', () => {
     expect(wrapper.text()).toBe('What planet was  born on?')
   })
-})
 
-describe('Test QuestionText component with Luke Skywalker person argument', () => {
-  test('the person prop should be Luke Skywalker', () => {
-    const wrapper = mount(QuestionText, {
-      propsData: {
-        person: 'Luke Skywalker'
-      }
-    })
-    expect(wrapper.props('person')).toBe('Luke Skywalker')
-  })
-
-  test('should be "What planet was Luke Skywalker born on?"', () => {
+  it('should render "What planet was Luke Skywalker born on?" when pass the Luke Skywalker as person', () => {
     const wrapper = mount(QuestionText, {
       propsData: {
         person: 'Luke Skywalker'
