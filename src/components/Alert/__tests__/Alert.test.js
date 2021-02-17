c86e8783d59a9668d52a8ef63c98492476fc3c97
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-02-16 22:33:07 -0300

    test(alert): add tests for Alert component

---
+++ src/components/Alert/__tests__/Alert.test.js
@@ -0,0 +1,41 @@
+import { mount } from '@vue/test-utils'
+
+import Alert from '..'
+
+describe('Alert component', () => {
+  it('should render the passed message', () => {
+    const message = 'Test message'
+    const wrapper = mount(Alert, {
+      propsData: {
+        message
+      }
+    })
+
+    expect(wrapper.text()).toBe(message)
+  })
+
+  it('should include the warning classes by default', () => {
+    const message = 'Test message'
+    const wrapper = mount(Alert, {
+      propsData: {
+        message
+      }
+    })
+
+    expect(wrapper.classes('bg-warning')).toBe(true)
+    expect(wrapper.classes('text-white')).toBe(true)
+  })
+
+  it('should include different classes with different type', () => {
+    const message = 'Test message'
+    const wrapper = mount(Alert, {
+      propsData: {
+        message,
+        type: 'negative'
+      }
+    })
+
+    expect(wrapper.classes('bg-negative')).toBe(true)
+    expect(wrapper.classes('text-white')).toBe(true)
+  })
+})
