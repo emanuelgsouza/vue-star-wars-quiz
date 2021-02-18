a4c99847955b0ec3ded7b85fc59c1cdcd22b8ad6
Emanuel Souza <emanuelgdesouza@gmail.com>
2021-02-17 22:48:05 -0300

    chore: install testing library cypress plugin

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// setup Testing Library with Cypress
+import '@testing-library/cypress/add-commands'
