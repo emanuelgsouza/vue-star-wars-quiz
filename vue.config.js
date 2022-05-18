f1c4813fcad516180e15f9c42f23a1158a4681fc
Emanuel Souza <emanuelgdesouza@gmail.com>
2022-05-18 17:43:37 -0300

    chore: remove @vue/compat

--- vue.config.js
+++ vue.config.js
@@ -6,23 +6,5 @@ module.exports = {
     workboxOptions: {
       skipWaiting: true
     }
-  },
-
-  chainWebpack: (config) => {
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
   }
 }
