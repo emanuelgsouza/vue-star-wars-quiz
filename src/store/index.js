1c1e749fd19e82937fb1e2adfde84a02b19ef04d
Emanuel Souza <emanuelgdesouza@gmail.com>
2022-05-18 17:40:02 -0300

    chore: upgrade vue-router and vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,11 @@
-import Vue from '@vue/compat'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 import state from './state'
 import mutations from './mutations'
 import plugins from './plugins'
 import * as actions from './actions'
 import * as getters from './getters'
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+export const store = createStore({
   state,
   actions,
   mutations,
